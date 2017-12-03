af555a1ea2fdb3e133823c912293e767c15a35dc
Soojeong <soojeongshin2@gmail.com>
2017-12-03 21:54:25 +0900

    Add drawable files

--- app/src/main/java/com/example/android/tourguide/ShopFragment.java
+++ app/src/main/java/com/example/android/tourguide/ShopFragment.java
@@ -45,33 +45,47 @@ public class ShopFragment extends Fragment {
      * Add attractions to the list of attractions
      */
     public void addAttractions() {
-        attractions.add(new Attraction(R.drawable.gyeongbokgung,
-                getString(R.string.myeongdong), getString(R.string.myeongdong_des),
-                getString(R.string.myeongdong_address),getString(R.string.myeongdong_transport),
+        attractions.add(new Attraction(R.drawable.myeongdong,
+                getString(R.string.myeongdong),
+                getString(R.string.myeongdong_des),
+                getString(R.string.myeongdong_address),
+                getString(R.string.myeongdong_transport),
                 getString(R.string.myeongdong_hours)));
-        attractions.add(new Attraction(R.drawable.gyeongbokgung,
-                getString(R.string.namdaemun), getString(R.string.namdaemun_des),
-                getString(R.string.namdaemun_address),getString(R.string.namdaemun_transport),
+        attractions.add(new Attraction(R.drawable.namdaemun,
+                getString(R.string.namdaemun),
+                getString(R.string.namdaemun_des),
+                getString(R.string.namdaemun_address),
+                getString(R.string.namdaemun_transport),
                 getString(R.string.namdaemun_hours)));
-        attractions.add(new Attraction(R.drawable.gyeongbokgung,
-                getString(R.string.goto_mall), getString(R.string.goto_mall_des),
-                getString(R.string.goto_mall_address),getString(R.string.goto_mall_transport),
+        attractions.add(new Attraction(R.drawable.goto_mall,
+                getString(R.string.goto_mall),
+                getString(R.string.goto_mall_des),
+                getString(R.string.goto_mall_address),
+                getString(R.string.goto_mall_transport),
                 getString(R.string.goto_mall_hours)));
-        attractions.add(new Attraction(R.drawable.gyeongbokgung,
-                getString(R.string.dongdaemun), getString(R.string.dongdaemun_des),
-                getString(R.string.dongdaemun_address),getString(R.string.dongdaemun_transport),
+        attractions.add(new Attraction(R.drawable.dongdaemun,
+                getString(R.string.dongdaemun),
+                getString(R.string.dongdaemun_des),
+                getString(R.string.dongdaemun_address),
+                getString(R.string.dongdaemun_transport),
                 getString(R.string.dongdaemun_hours)));
-        attractions.add(new Attraction(R.drawable.gyeongbokgung,
-                getString(R.string.common_ground), getString(R.string.common_ground_des),
-                getString(R.string.common_ground_address),getString(R.string.common_ground_transport),
+        attractions.add(new Attraction(R.drawable.common_ground,
+                getString(R.string.common_ground),
+                getString(R.string.common_ground_des),
+                getString(R.string.common_ground_address),
+                getString(R.string.common_ground_transport),
                 getString(R.string.common_ground_hours)));
-        attractions.add(new Attraction(R.drawable.gyeongbokgung,
-                getString(R.string.garosu_gil), getString(R.string.garosu_gil_des),
-                getString(R.string.garosu_gil_address),getString(R.string.garosu_gil_transport),
+        attractions.add(new Attraction(R.drawable.garosugil,
+                getString(R.string.garosu_gil),
+                getString(R.string.garosu_gil_des),
+                getString(R.string.garosu_gil_address),
+                getString(R.string.garosu_gil_transport),
                 getString(R.string.garosu_gil_hours)));
-        attractions.add(new Attraction(R.drawable.gyeongbokgung,
-                getString(R.string.times_square), getString(R.string.times_square_des),
-                getString(R.string.times_square_address),getString(R.string.times_square_transport),
+        attractions.add(new Attraction(R.drawable.times_square,
+                getString(R.string.times_square),
+                getString(R.string.times_square_des),
+                getString(R.string.times_square_address),
+                getString(R.string.times_square_transport),
                 getString(R.string.times_square_hours)));
 
     }
